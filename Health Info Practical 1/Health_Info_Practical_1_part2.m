1e4272d299d2de0f3ad936f227919851867aaf3d
bmenn <bmenn@server.com>
2013-09-26 17:12:41 -0400

    Add comments about rdann errors

--- Health Info Practical 1/Health_Info_Practical_1_part2.m
+++ Health Info Practical 1/Health_Info_Practical_1_part2.m
@@ -1,7 +1,23 @@
-[tm, signal, fs] = rdsamp('chfdb/chf01',1, 0, 2);
-[ann] = rdann('chfdb/chf01');
+[tm, signal, fs] = rdsamp('chfdb/chf01',[], 1000, 1);
+% The following line doesn't work and is suppose to fetch the annotations
+%
+%   ann = rdann('chfdb/chf01', 'ecg', 3, 1000, 1);
+%
+% Returns 'Improper assignment with rectangular empty matrix'
+%
+% However when we test:
+%  
+%   rdann('mitdb/100', 'atr'
+%
+% and
+% 
+%   rdann('challenge/2013/set-a/a01', 'fqrs')
+%
+% it works.
 
-plot(tm, signal(:,1), 'r', tm, signal(:,2), 'b')
-hold on
-plot(tm(ann), signal(ann, 1), 'ro', MarkerSize', 4, ...
-     tm(ann), signal(ann, 2), 'bo', MarkerSize', 4)
+plot(tm, signal(:,1), 'r', tm, signal(:,2) - 10, 'b')
+% How we would plot the annotations if we had them
+%
+%   hold on
+%   plot(tm(ann), signal(ann, 1), 'ro', 'MarkerSize', 4, ...
+%       tm(ann), signal(ann, 2) - 10, 'bo', 'MarkerSize', 4)
