e2106d6d239e280140b54f41418b023c799e9e61
Ben Schmaltz <benschmaltz@gmail.com>
2013-10-16 23:25:18 -0400

    totally done with part 1 of imaging (fixed 2-d HS space), part 2 is still not right

--- Imaging Practical 2/Imaging_Practical_2_part2.m
+++ Imaging Practical 2/Imaging_Practical_2_part2.m
@@ -6,8 +6,8 @@ gray_dct = dct2(gray);
 gray_idct = idct2(gray_dct(1:128, 1:128), 512, 512);
 residual_dct = abs(gray-gray_idct);
 
-gray_dft = fft2(gray);
-gray_idft = ifft2(gray_dft(192:319, 192:319), 512, 512);
+gray_dft = imag(fft2(gray));
+gray_idft = ifft2(imresize(gray_dft, 0.25), 512, 512);
 residual_dft = abs(gray-gray_idft);
 
 figure, imshow(gray),title('Grayscale 512x512');
@@ -16,5 +16,5 @@ figure, imshow(gray_idct),title('Inverse DCT Image');
 figure, imshow(residual_dct),title('DCT: Difference Image');
 
 figure, imshow(gray_dft),title('DFT');
-%figure, imshow(gray_idft),title('Inverse DFT Image');
-%figure, imshow(residual_dft),title('DFT: Difference Image');
+figure, imshow(gray_idft),title('Inverse DFT Image');
+figure, imshow(residual_dft),title('DFT: Difference Image');
