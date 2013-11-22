fd1c24360f3c586ebf1a0805b8c4bdca2f00b460
bmenn <bmenn@server.com>
2013-11-21 23:33:41 -0500

    Added Gener_Accum

--- Imaging Practical 3/Detect_Peaks.m
+++ Imaging Practical 3/Detect_Peaks.m
@@ -1,5 +1,13 @@
 function Hough_Peaks = Detect_Peaks (Hough_Accum, R_range)
-
-    Hough_Peaks=0;
+    xy_margin = 15;
+    R_margin = 15;
+    Npeaks = 15;
+    
+    thres = max(Hough_Accum(:)) * 0.5;
+    [h w d] = size(Hough_Accum);
+    
+    for i = 1:d
+        find
+        
 end
 
--- Imaging Practical 3/Generate_Accum.m
+++ Imaging Practical 3/Generate_Accum.m
@@ -1,12 +1,27 @@
 function Hough_Accum=Generate_Accum(Edge_image, Min_R, Max_R)
-    Hough_Accum=zeros(size(Edge_image,1), size(Edge_image,2), Max_R-Min_R+1);
-    rrange=zeros(Max_R,Min_R);
     [x y] = ind2sub(size(Edge_image), find(Edge_image(:) == 1));
-    for i=Min_R:Max_R
-        
-        rrange(i-Min_R+1)=int8(i);
-    end
-    % get indices of non-zero features of blue
-    [featR, featC] = find(Edge_image);
-    
+    [h w] = size(Edge_image);
+    theta = linspace(0, 2 * pi(), 500);
+    R = Min_R:Max_R;
+
+    Hough_Accum=zeros(h, w, length(R));
+    for i = 1:length(R)
+        for j = 1:length(x)
+            r = R(i) * ones(1, 500);
+            [cirx ciry] = pol2cart(theta, r);
+            cirx = round(cirx + x(j));
+            ciry = round(ciry + y(j));
+            
+            % Remove pixels outside of image
+            ciry = ciry(cirx > 0 & cirx <= w);
+            cirx = cirx(cirx > 0 & cirx <= w);
+            cirx = cirx(ciry > 0 & ciry <= h);
+            ciry = ciry(ciry > 0 & ciry <= h);
+            
+            for k = 1:length(cirx)
+                Hough_Accum(ciry(k),cirx(k),i) = Hough_Accum(ciry(k),cirx(k),i) + 1/R(i);
+            end
+        end
+        %rrange(i-Min_R+1)=int8(i);
+    end   
 end
