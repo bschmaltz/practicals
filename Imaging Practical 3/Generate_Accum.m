22c689b703137eb238eafcc538101ffa6a2cca74
bmenn <bmenn@server.com>
2013-11-21 23:06:59 -0500

    Determined xy coords of edges

--- Imaging Practical 3/Generate_Accum.m
+++ Imaging Practical 3/Generate_Accum.m
@@ -1,7 +1,9 @@
 function Hough_Accum=Generate_Accum(Edge_image, Min_R, Max_R)
     Hough_Accum=zeros(size(Edge_image,1), size(Edge_image,2), Max_R-Min_R+1);
     rrange=zeros(Max_R,Min_R);
+    [x y] = ind2sub(size(Edge_image), find(Edge_image(:) == 1));
     for i=Min_R:Max_R
+        
         rrange(i-Min_R+1)=int8(i);
     end
     % get indices of non-zero features of blue
